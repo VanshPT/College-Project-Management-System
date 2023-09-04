8cc35dc487bf64a1460cb71f4685335d2383c136
Vansh <vanshpthakkar@gmail.com>
2023-09-04 19:59:28 +0530

    Project model created to store project details of a particular student and database connected with form to store form data successfully in database

--- pmsStudent/models.py
+++ pmsStudent/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from pmsMain.models import Student
 
 # Create your models here.
+class Project(models.Model):
+    project_id = models.AutoField(primary_key=True)
+    roll_no = models.ForeignKey(Student, on_delete=models.CASCADE)
+    project_name = models.CharField(max_length=100)
+    member1 = models.ForeignKey(Student, related_name='member1_projects', on_delete=models.CASCADE ,null=True ,blank=True)
+    member2 = models.ForeignKey(Student, related_name='member2_projects', on_delete=models.CASCADE ,null=True ,blank=True)
+    member3 = models.ForeignKey(Student, related_name='member3_projects', on_delete=models.CASCADE ,null=True ,blank=True)
+    member4 = models.ForeignKey(Student, related_name='member4_projects', on_delete=models.CASCADE ,null=True ,blank=True)
+    project_description = models.TextField(max_length=1000)
+    github_link = models.URLField(max_length=100)
+    
+    PROJECT_TYPE_CHOICES = [
+        ('full_year', 'Full Year'),
+        ('semester', 'Semester'),
+        ('personal', 'Personal'),
+    ]
+    project_type = models.CharField(
+        max_length=20,
+        choices=PROJECT_TYPE_CHOICES,
+    )
+    
+    project_semesters = models.CharField(max_length=50)
+    start_date = models.DateField()
+    end_date = models.DateField()
+
+    def __str__(self):
+        return self.project_name
--- pmsStudent/views.py
+++ pmsStudent/views.py
@@ -1,28 +1,75 @@
 from django.shortcuts import render
 from django.http import HttpResponse,HttpResponseRedirect
 from pmsMain.models import Student,Team,Mentor
+from .models import Project
 from django.urls import reverse
 # Create your views here.
 def index(request,roll_no):
+    student_user = Student.objects.filter(roll_no=roll_no).first()
     if request.method == 'POST':
         
-        roll_number=request.POST.get('roll_no','')
-        project_name=request.POST.get('projectName','')
+        roll_number=request.POST.get('rollno')
+        try:
+            student_instance = Student.objects.get(roll_no=roll_number)
+        except Student.DoesNotExist:
+            error_message = "Invalid roll number for project creator."
+            print(error_message)
+            return render(request, 'pmsStudent/index.html', {'student': student_user, 'error_message': error_message})
+
+        project_name=request.POST.get('projectName')
+
         team_member1=request.POST.get('member1','')
         team_member2=request.POST.get('member2','')
         team_member3=request.POST.get('member3','')
         team_member4=request.POST.get('member4','')
+
+        try:
+            student_member1 = Student.objects.get(roll_no=team_member1)
+            student_member2 = Student.objects.get(roll_no=team_member2)
+            student_member3 = Student.objects.get(roll_no=team_member3)
+            student_member4 = Student.objects.get(roll_no=team_member4)
+        except Student.DoesNotExist:
+            error_message = "Invalid roll numbers for team members."
+            print(error_message)
+            return render(request, 'pmsStudent/index.html', {'student': student_user, 'error_message': error_message})
+
         project_desc=request.POST.get('projectDescription','')
         github_link=request.POST.get('githubLink','')
-        project_type=request.POST.get('proj_type','')
+        project_type=request.POST.get('projectType','')
         semester_values = {}
         for i in range(1, 9):
             key = f'semester{i}' 
             value = request.POST.get(key, '')  
-            semester_values[key] = value
+            if value=='on':
+                semester_values[key] = value
+        
         start_date=request.POST.get('startDate','')
         end_date=request.POST.get('endDate','')
-        print(roll_number)
-        print(semester_values)
-    student_user = Student.objects.filter(roll_no=roll_no).first()
+        roll=[]
+        roll_numbers = Student.objects.values('roll_no')
+        for j in roll_numbers:
+            roll.append(j['roll_no'])
+            
+        if (team_member1 not in roll) or (team_member2 not in roll) or (team_member3 not in roll) or (team_member4 not in roll) or (roll_number not in roll):
+            error_message = "Invalid roll numbers for team members or project creator."
+            print(error_message)
+            return render(request, 'pmsStudent/index.html', {'student': student_user, 'error_message': error_message})
+        projects = Project(
+        roll_no=student_instance,
+        project_name=project_name,
+        member1=student_member1,
+        member2=student_member2,
+        member3=student_member3,
+        member4=student_member4,
+        project_description=project_desc,
+        github_link=github_link,
+        project_type=project_type,
+        project_semesters=semester_values,
+        start_date=start_date,
+        end_date=end_date,
+        )
+        projects.save()
+    
+
+    # retrieval of data from database and show on web page code below
     return render(request, 'pmsStudent/index.html', {'student': student_user})
